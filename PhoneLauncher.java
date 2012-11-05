70021528b82a51790efb03944c142c5f7b094ca0
Alex <a.rokotas@gmail.com>
2012-11-05 15:30:30 +0000

    first 2 exercises working correctly :)

---
+++ PhoneLauncher.java
@@ -0,0 +1,31 @@
+public class PhoneLauncher {
+	public static void main(String[] args) {
+	PhoneLauncher launcher = new PhoneLauncher();
+	launcher.launch();
+	}
+	
+	public void launch() {
+	SmartPhone myMobile = new SmartPhone();
+	myMobile.call("01");
+	myMobile.call("02");
+	myMobile.call("03");
+	myMobile.call("04");
+	myMobile.call("05");
+	myMobile.call("06");
+	myMobile.call("07");
+	myMobile.call("08");
+	myMobile.call("09");
+	myMobile.call("00446925125325");
+	myMobile.call("10");
+	myMobile.call("11");
+	myMobile.call("12");
+	
+	myMobile.ringAlarm("beeeeeeeeeeep");
+	myMobile.playGame("tetris");
+	myMobile.printLastNumbers();
+	myMobile.browseWeb("google");
+	System.out.println(myMobile.findPosition());
+	}
+
+	
+}
---
+++ SmartPhone.java
@@ -0,0 +1,22 @@
+public class SmartPhone extends MobilePhone {
+	
+	public void browseWeb(String website) {
+		System.out.println("Connecting to " + website);
+	}	
+	
+	public String findPosition() {
+		return "I found you, you are ....";
+	}	
+	
+	@Override
+	public void call(String number) {
+		if (number.substring(0,2).equals("00")) {
+			System.out.println("Calling " + number + " through the internet to save money");
+		} else {
+			super.call(number);
+		  }	
+			
+	}	
+			
+	
+}	
